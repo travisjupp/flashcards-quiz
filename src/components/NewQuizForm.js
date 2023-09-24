5a8aaa9e486b486e990ce54f3dcede5abd2a0bc8
tjupp <travisjupp@gmail.com>
2023-09-24 17:29:33 -0600

    implement topics selector to new quiz form

--- src/components/NewQuizForm.js
+++ src/components/NewQuizForm.js
@@ -4,13 +4,15 @@ import { useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
 // import selectors
+import { selectTopics } from "../features/topics/topicsSlice";
+import { addQuiz } from "../features/quizzes/quizzesSlice";
 
 export default function NewQuizForm() {
   const [name, setName] = useState("");
   const [cards, setCards] = useState([]);
   const [topicId, setTopicId] = useState("");
   const navigate = useNavigate();
-  const topics = {};  // Replace with topics 
+  const topics = useSelector(selectTopics);  // Replace with topics 
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
@@ -27,7 +29,12 @@ export default function NewQuizForm() {
     const quizId = uuidv4();
 
     // dispatch add quiz action 
-
+dispatch(addQuiz({
+  id: uuidv4(),
+  name: name,
+  topicId: topicId,
+  cardIds: []  
+}));
     navigate(ROUTES.quizzesRoute())
   };
 
