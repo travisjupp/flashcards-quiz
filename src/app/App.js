4f3ba6ff6f847662a4515f9f0fc9772018f1deb8
tjupp <travisjupp@gmail.com>
2023-10-28 16:09:50 -0600

    format code

--- src/app/App.js
+++ src/app/App.js
@@ -16,14 +16,14 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<AppLayout/>}>
-    <Route path="topics" element={<Topics/>}/>
-    <Route path="topics/new" element={<NewTopicForm/>}/>
-    <Route path="topics/:topicId" element={<Topic/>}/>
-    <Route path="quizzes" element={<Quizzes/>}/>
-    <Route path="quizzes/new" element={<NewQuizForm/>}/>
-    <Route path="quizzes/:quizId" element={<Quiz/>}/>
-  </Route>
+        <Route path="/" element={<AppLayout />}>
+          <Route path="topics" element={<Topics />} />
+          <Route path="topics/new" element={<NewTopicForm />} />
+          <Route path="topics/:topicId" element={<Topic />} />
+          <Route path="quizzes" element={<Quizzes />} />
+          <Route path="quizzes/new" element={<NewQuizForm />} />
+          <Route path="quizzes/:quizId" element={<Quiz />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
